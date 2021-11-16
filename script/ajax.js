a81138ccb58a86f8b8b4f8cda1d4cf9a8ba3a47f
Deadline <59339504+Alkohole@users.noreply.github.com>
2021-11-16 06:25:00 +0300

    sss

--- script/ajax.js
+++ script/ajax.js
@@ -2,7 +2,7 @@ $('#submitBtn').click(function(e){
   e.preventDefault();
     var formData = $(this).parent('form').serialize();
      $.ajax({
-       url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSfD7qfNagDlYAtcfDzE4gVdiwsbj3E0NYcDlGlzi1lVA-wofw/formResponse',
+       url: 'https://docs.google.com/forms/d/e/1FAIpQLScSTf9w08H3g6gmSkwp2BKy4DfHPeOcHX-MbK3fmPj0yXP39g',
        method: 'POST',
        data: formData,
        statusCode: {
