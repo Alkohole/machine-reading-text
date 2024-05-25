fe8f3278a5f6529f2cc64e8d6cf6b191560cf40c
Alkohole <59339504+Alkohole@users.noreply.github.com>
2024-05-25 19:37:45 +0200

    Update mrt.user.js
    Добавлена возможность изменять позицию окно по горизонтали

--- mrt.user.js
+++ mrt.user.js
@@ -2,7 +2,7 @@
 // @name         [MRT] - Machine reading text
 // @name:ru      [MRT] - Машинное чтение текста
 // @namespace    http://tampermonkey.net/
-// @version      0.5.1
+// @version      0.5.3
 // @description  Display custom HTML page from URL on button click with settings panel
 // @author       Alkohole
 // @match        *://*/*
@@ -20,12 +20,16 @@
 
     var customButton = document.getElementById('customButton');
     var customIframe = document.getElementById('customIframe');
+    var customSet = document.getElementById('customSet');
+
+    var offsetRight = 0; // измените это значение, чтобы установить уровень сдвига по горизонтали для умолчания
+    var charPos = "right"; // переключение позиционирования left/right для умолчания
 
     if (customButton && customIframe) {
-
         if (window.top !== window.self) {
             document.body.removeChild(customButton);
             document.body.removeChild(customIframe);
+            document.body.removeChild(customSet);
         }
         return;
     }
@@ -38,7 +42,7 @@
     button.style.fontSize = "20px";
     button.style.position = "fixed";
     button.style.bottom = "10px";
-    button.style.right = "10px";
+    button.style[charPos] = `calc(10px + ${offsetRight}px)`;
     button.style.width = "30px";
     button.style.height = "30px";
     button.style.borderRadius = "50%";
@@ -58,45 +62,35 @@
     toggleButton.style.borderRadius = "15px";
     toggleButton.style.border = "none";
     toggleButton.style.position = "fixed";
-    toggleButton.style.right = "130px";
+    toggleButton.style[charPos] = `calc(130px + ${offsetRight}px)`;
     toggleButton.style.zIndex = 9998;
     toggleButton.style.backgroundColor = "rgb(78, 78, 78)";
     toggleButton.style.padding = "5px 10px";
     toggleButton.style.cursor = "pointer";
     toggleButton.style.transition = "bottom 0.5s ease";
 
-    function adjustToggleButtonPosition() {
-        if (iframe.style.height === "460px") {
-            toggleButton.style.display = "block";
-            toggleButton.style.bottom = "453px";
-            button.style.bottom = "440px";
-            button.style.right = "5px";
-        } else {
-            toggleButton.style.display = "block";
-            toggleButton.style.bottom = "110px";
-            button.style.bottom = "95px";
-            button.style.right = "5px";
+    var settingsButton = document.createElement("button");
+    settingsButton.id = 'customSet';
+    settingsButton.textContent = "⚙";
+    settingsButton.style.position = "fixed";
+    settingsButton.style.display = "none";
+    settingsButton.style.opacity = 0;
+    settingsButton.style.bottom = "10px";
+    settingsButton.style.width = "30px";
+    settingsButton.style.height = "30px";
+    settingsButton.style.borderRadius = "50%";
+    settingsButton.style.border = "none";
+    settingsButton.style[charPos] = `calc(50px + ${offsetRight}px)`;
+    settingsButton.style.zIndex = 9999;
+    settingsButton.style.cursor = "pointer";
+    settingsButton.style.transition = "height 0.5s ease, opacity 0.5s ease";
+    settingsButton.addEventListener("click", function() {
+        var newOffset = prompt("Введите новое значение отступа справа (в пикселях):");
+        if (newOffset !== null) {
+            changeOffsetRight(parseInt(newOffset));
         }
-    }
-
-    function handleIframeDisplayChange() {
-        if (iframe.style.display === "none" || iframe.style.display === "" || getComputedStyle(iframe).opacity === "1") {
-            toggleButton.style.display = "none";
-            button.style.bottom = "10px";
-            button.style.right = "10px";
-        } else {
-            adjustToggleButtonPosition();
-        }
-    }
-    toggleButton.addEventListener("click", function() {
-        if (iframe.style.height === "460px") {
-            iframe.style.height = "120px";
-        } else {
-            iframe.style.height = "460px";
-        }
-        adjustToggleButtonPosition();
     });
-    document.body.appendChild(toggleButton);
+    document.body.appendChild(settingsButton);
 
     var iframe = document.createElement("iframe");
     iframe.id = 'customIframe';
@@ -104,7 +98,7 @@
     iframe.style.height = "460px";
     iframe.style.position = "fixed";
     iframe.style.bottom = "0px";
-    iframe.style.right = "5px";
+    iframe.style[charPos] = `calc(5px + ${offsetRight}px)`;
     iframe.style.zIndex = 9997;
     iframe.style.display = "none";
     iframe.style.border = "none";
@@ -115,21 +109,82 @@
     button.addEventListener("click", function() {
         if (iframe.style.display === "none" || iframe.style.display === "") {
             iframe.style.display = "block";
+            settingsButton.style.display = "block";
             setTimeout(function() {
+                settingsButton.style.opacity = 1;
                 iframe.style.opacity = 1;
             }, 0);
         } else {
             iframe.style.opacity = 0;
+            settingsButton.style.opacity = 0;
             setTimeout(function() {
                 iframe.style.display = "none";
+                settingsButton.style.display = "none";
             }, 500);
         }
         handleIframeDisplayChange();
     });
+
+    function adjustToggleButtonPosition() {
+        if (iframe.style.height === "460px") {
+            toggleButton.style.display = "block";
+            toggleButton.style.bottom = "453px";
+            button.style.bottom = "445px";
+            button.style[charPos] = `calc(10px + ${offsetRight}px)`;
+            settingsButton.style.bottom = "445px";
+            settingsButton.style[charPos] = `calc(50px + ${offsetRight}px)`;
+        } else {
+            toggleButton.style.display = "block";
+            toggleButton.style.bottom = "110px";
+            button.style.bottom = "100px";
+            button.style[charPos] = `calc(10px + ${offsetRight}px)`;
+            settingsButton.style.bottom = "100px";
+            settingsButton.style[charPos] = `calc(50px + ${offsetRight}px)`;
+        }
+    }
+
+    function adjustPositions() {
+        button.style[charPos] = `calc(10px + ${offsetRight}px)`;
+        iframe.style[charPos] = `calc(5px + ${offsetRight}px)`;
+        toggleButton.style[charPos] = `calc(130px + ${offsetRight}px)`;
+        settingsButton.style[charPos] = `calc(10px + ${offsetRight}px)`;
+        adjustToggleButtonPosition();
+    }
+
+    function handleIframeDisplayChange() {
+        if (iframe.style.display === "none" || iframe.style.display === "" || getComputedStyle(iframe).opacity === "1") {
+            toggleButton.style.display = "none";
+            button.style.bottom = "10px";
+            button.style[charPos] = `calc(10px + ${offsetRight}px)`;
+            settingsButton.style.bottom = "10px";
+            settingsButton.style[charPos] = `calc(50px + ${offsetRight}px)`;
+        } else {
+            adjustToggleButtonPosition();
+        }
+    }
+
+    toggleButton.addEventListener("click", function() {
+        if (iframe.style.height === "460px") {
+            iframe.style.height = "120px";
+        } else {
+            iframe.style.height = "460px";
+        }
+        adjustToggleButtonPosition();
+    });
+
+    document.body.appendChild(toggleButton);
+
     if (window.top === window.self) {
         document.body.appendChild(button);
         document.body.appendChild(iframe);
     }
+
     document.body.appendChild(iframe);
+    adjustPositions();
     handleIframeDisplayChange();
+
+    function changeOffsetRight(value) {
+        offsetRight = value;
+        adjustPositions();
+    }
 })();
