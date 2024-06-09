680cc10ebb3280dc628a6bbc7de56536fe2c705c
Alkohole <59339504+Alkohole@users.noreply.github.com>
2024-06-09 20:03:32 +0200

    Update mrt.user.js

--- mrt.user.js
+++ mrt.user.js
@@ -2,7 +2,7 @@
 // @name         [MRT] - Machine reading text
 // @name:ru      [MRT] - Машинное чтение текста
 // @namespace    http://tampermonkey.net/
-// @version      0.5.3
+// @version      0.5.4
 // @description  Display custom HTML page from URL on button click with settings panel
 // @author       Alkohole
 // @match        *://*/*
@@ -22,10 +22,33 @@
     var customIframe = document.getElementById('customIframe');
     var customSet = document.getElementById('customSet');
 
-    var offsetRight = 0; // измените это значение, чтобы установить уровень сдвига по горизонтали для умолчания
+    var offset = 0; // измените это значение, чтобы установить уровень сдвига по горизонтали для умолчания
     var charPos = "right"; // переключение позиционирования left/right для умолчания
 
-    if (customButton && customIframe) {
+    var shadowHost = document.createElement('div');
+    shadowHost.style.position = 'fixed';
+    shadowHost.style.zIndex = 10000;
+    document.body.appendChild(shadowHost);
+
+    var shadowRoot = shadowHost.attachShadow({ mode: 'open' });
+
+    var style = document.createElement('style');
+    style.innerHTML = `
+    #customButton,
+    #toggleButton,
+    #customSet {
+        outline: none;
+        &&:focus {
+            outline: none;
+        }
+        &&:active {
+            outline: none;
+        }
+    }
+    `;
+    shadowRoot.appendChild(style);
+
+    if (customButton && customIframe && customSet) {
         if (window.top !== window.self) {
             document.body.removeChild(customButton);
             document.body.removeChild(customIframe);
@@ -42,7 +65,7 @@
     button.style.fontSize = "20px";
     button.style.position = "fixed";
     button.style.bottom = "10px";
-    button.style[charPos] = `calc(10px + ${offsetRight}px)`;
+    button.style[charPos] = `calc(10px + ${offset}px)`;
     button.style.width = "30px";
     button.style.height = "30px";
     button.style.borderRadius = "50%";
@@ -62,7 +85,7 @@
     toggleButton.style.borderRadius = "15px";
     toggleButton.style.border = "none";
     toggleButton.style.position = "fixed";
-    toggleButton.style[charPos] = `calc(130px + ${offsetRight}px)`;
+    toggleButton.style[charPos] = `calc(130px + ${offset}px)`;
     toggleButton.style.zIndex = 9998;
     toggleButton.style.backgroundColor = "rgb(78, 78, 78)";
     toggleButton.style.padding = "5px 10px";
@@ -71,26 +94,31 @@
 
     var settingsButton = document.createElement("button");
     settingsButton.id = 'customSet';
-    settingsButton.textContent = "⚙";
+    settingsButton.style.backgroundImage = 'url("https://cdn-icons-png.flaticon.com/256/2040/2040504.png")';
+    settingsButton.style.backgroundSize = 'cover';
+    settingsButton.style.backgroundColor = "rgb(248, 233, 214)";
     settingsButton.style.position = "fixed";
     settingsButton.style.display = "none";
-    settingsButton.style.opacity = 0;
+    settingsButton.style.opacity = 0.1;
     settingsButton.style.bottom = "10px";
     settingsButton.style.width = "30px";
     settingsButton.style.height = "30px";
     settingsButton.style.borderRadius = "50%";
     settingsButton.style.border = "none";
-    settingsButton.style[charPos] = `calc(50px + ${offsetRight}px)`;
+    settingsButton.style[charPos] = `calc(50px + ${offset}px)`;
     settingsButton.style.zIndex = 9999;
+    settingsButton.style.padding = "0";
     settingsButton.style.cursor = "pointer";
-    settingsButton.style.transition = "height 0.5s ease, opacity 0.5s ease";
+    settingsButton.style.transition = "opacity 0.5s ease";
+    settingsButton.onmouseover = function() { this.style.opacity = 0.8; }
+    settingsButton.onmouseout = function() { this.style.opacity = 0.1; }
     settingsButton.addEventListener("click", function() {
-        var newOffset = prompt("Введите новое значение отступа (px не указывать):");
+        var newOffset = prompt("Введите новое значение отступа в px(без указания px):");
         if (newOffset !== null) {
-            changeOffsetRight(parseInt(newOffset));
+            changeOffset(parseInt(newOffset));
         }
     });
-    document.body.appendChild(settingsButton);
+    shadowRoot.appendChild(settingsButton);
 
     var iframe = document.createElement("iframe");
     iframe.id = 'customIframe';
@@ -98,21 +126,21 @@
     iframe.style.height = "460px";
     iframe.style.position = "fixed";
     iframe.style.bottom = "0px";
-    iframe.style[charPos] = `calc(5px + ${offsetRight}px)`;
+    iframe.style[charPos] = `calc(5px + ${offset}px)`;
     iframe.style.zIndex = 9997;
     iframe.style.display = "none";
     iframe.style.border = "none";
     iframe.style.opacity = 0;
     iframe.style.transition = "height 0.5s ease, opacity 0.5s ease";
-    iframe.src = "https://alkohole.github.io/machine-reading-text/index.html"
+    iframe.src = "https://alkohole.github.io/machine-reading-text/index.html";
 
     button.addEventListener("click", function() {
         if (iframe.style.display === "none" || iframe.style.display === "") {
             iframe.style.display = "block";
             settingsButton.style.display = "block";
             setTimeout(function() {
-                settingsButton.style.opacity = 1;
                 iframe.style.opacity = 1;
+                settingsButton.style.opacity = 0.1;
             }, 0);
         } else {
             iframe.style.opacity = 0;
@@ -130,24 +158,24 @@
             toggleButton.style.display = "block";
             toggleButton.style.bottom = "453px";
             button.style.bottom = "445px";
-            button.style[charPos] = `calc(10px + ${offsetRight}px)`;
+            button.style[charPos] = `calc(10px + ${offset}px)`;
             settingsButton.style.bottom = "445px";
-            settingsButton.style[charPos] = `calc(50px + ${offsetRight}px)`;
+            settingsButton.style[charPos] = `calc(50px + ${offset}px)`;
         } else {
             toggleButton.style.display = "block";
             toggleButton.style.bottom = "110px";
             button.style.bottom = "100px";
-            button.style[charPos] = `calc(10px + ${offsetRight}px)`;
+            button.style[charPos] = `calc(10px + ${offset}px)`;
             settingsButton.style.bottom = "100px";
-            settingsButton.style[charPos] = `calc(50px + ${offsetRight}px)`;
+            settingsButton.style[charPos] = `calc(50px + ${offset}px)`;
         }
     }
 
     function adjustPositions() {
-        button.style[charPos] = `calc(10px + ${offsetRight}px)`;
-        iframe.style[charPos] = `calc(5px + ${offsetRight}px)`;
-        toggleButton.style[charPos] = `calc(130px + ${offsetRight}px)`;
-        settingsButton.style[charPos] = `calc(10px + ${offsetRight}px)`;
+        button.style[charPos] = `calc(10px + ${offset}px)`;
+        iframe.style[charPos] = `calc(5px + ${offset}px)`;
+        toggleButton.style[charPos] = `calc(130px + ${offset}px)`;
+        settingsButton.style[charPos] = `calc(10px + ${offset}px)`;
         adjustToggleButtonPosition();
     }
 
@@ -155,9 +183,9 @@
         if (iframe.style.display === "none" || iframe.style.display === "" || getComputedStyle(iframe).opacity === "1") {
             toggleButton.style.display = "none";
             button.style.bottom = "10px";
-            button.style[charPos] = `calc(10px + ${offsetRight}px)`;
+            button.style[charPos] = `calc(10px + ${offset}px)`;
             settingsButton.style.bottom = "10px";
-            settingsButton.style[charPos] = `calc(50px + ${offsetRight}px)`;
+            settingsButton.style[charPos] = `calc(50px + ${offset}px)`;
         } else {
             adjustToggleButtonPosition();
         }
@@ -172,19 +200,19 @@
         adjustToggleButtonPosition();
     });
 
-    document.body.appendChild(toggleButton);
+    shadowRoot.appendChild(toggleButton);
 
     if (window.top === window.self) {
-        document.body.appendChild(button);
-        document.body.appendChild(iframe);
+        shadowRoot.appendChild(button);
+        shadowRoot.appendChild(iframe);
     }
 
-    document.body.appendChild(iframe);
+    shadowRoot.appendChild(iframe);
     adjustPositions();
     handleIframeDisplayChange();
 
-    function changeOffsetRight(value) {
-        offsetRight = value;
+    function changeOffset(value) {
+        offset = value;
         adjustPositions();
     }
 })();
